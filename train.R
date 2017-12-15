6ff867229770b1b1f95d25952b8f16debdd55a80
Eduardo Lago Aguilar <eduardo.lago.aguilar@gmail.com>
2017-12-15 14:52:31 -0300

    added README, makign train.R executable script

--- train.R
+++ train.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 heroes <- c("Alchemist", "Ancient Apparition", "Anti-Mage", "Axe", "Bane", "Batrider", "Beastmaster", "Bloodseeker", "Bounty Hunter", "Brewmaster", "Broodmother", "Centaur Warrunner", "Chaos Knight", "Chen", "Clinkz", "Clockwerk", "Crystal Maiden", "Dark Seer", "Dazzle", "Death Prophet", "Disruptor", "Doom", "Dragon Knight", "Drow Ranger", "Earthshaker", "Enchantress", "Enigma", "Faceless Void", "Gyrocopter", "Huskar", "Invoker", "Jakiro", "Juggernaut", "Keeper of the Light", "Kunkka", "Leshrac", "Lich", "Lifestealer", "Lina", "Lion", "Lone Druid", "Luna", "Lycanthrope", "Magnus", "Medusa", "Meepo", "Mirana", "Morphling", "Naga Siren", "Nature's Prophet", "Necrolyte", "Night Stalker", "Nyx Assassin", "Ogre Magi", "Omniknight", "Outworld Devourer", "Phantom Assassin", "Phantom Lancer", "Puck", "Pudge", "Pugna", "Queen of Pain", "Razor", "Riki", "Rubick", "Sand King", "Shadow Demon", "Shadow Fiend", "Shadow Shaman", "Silencer", "Skeleton King", "Slardar", "Slark", "Sniper", "Spectre", "Spirit Breaker", "Storm Spirit", "Sven", "Templar Assassin", "Tidehunter", "Timbersaw", "Tinker", "Tiny", "Treant Protector", "Troll Warlord", "Undying", "Ursa", "Vengeful Spirit", "Venomancer", "Viper", "Visage", "Warlock", "Weaver", "Windrunner", "Wisp", "Witch Doctor", "Zeus")
 num_heroes = length(heroes)
 search_hero <- function (hero) {
@@ -83,4 +85,4 @@ cat("\nhits: ", paste(hits))
 cat("\nmiss: ", paste(miss))
 cat("\ntotal: ", paste(N))
 cat("\nhit ratio: ", paste(100 * hits / N), "%")
-cat("\nscore: ", paste(100 * (hits - miss) / N))
+cat("\nhackerrank score: ", paste(100 * (hits - miss) / N))
